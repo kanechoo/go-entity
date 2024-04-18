b3274f0cafbb4b8ef17bcb9406af6eeb8f5c7d17
QG <konworking@gmail.com>
2024-04-18 16:09:31 +0800

    rename

--- entity/entity.go
+++ entity/entity.go
@@ -4,44 +4,44 @@ import (
 	"time"
 )
 
-// IPScanTask  ip_scan_tasks 表
-type IPScanTask struct {
-	ID         int       `files:"id"`          // 记录ID
-	TaskName   string    `files:"task_name"`   // 任务名称
-	StartTime  time.Time `files:"start_time"`  // 任务开始时间
-	EndTime    time.Time `files:"end_time"`    // 任务接收时间
-	ASN        string    `files:"asn"`         // ASN编号
-	Name       string    `files:"name"`        // ASN名称
-	Netblock   string    `files:"netblock"`    // ASN的网段
-	NumIPs     int       `files:"num_ips"`     // IP数量
-	Country    string    `files:"country"`     // ASN所属国家编码
-	Domain     string    `files:"domain"`      // ASN所属者的域名
-	CreateTime time.Time `files:"create_time"` // 任务的创建时间
-	Runtime    int       `files:"run_time"`    // 任务的运行时间(单位:秒)
+// IPTask  ip_scan_tasks 表
+type IPTask struct {
+	ID         int       `json:"id"`          // 记录ID
+	TaskName   string    `json:"task_name"`   // 任务名称
+	StartTime  time.Time `json:"start_time"`  // 任务开始时间
+	EndTime    time.Time `json:"end_time"`    // 任务接收时间
+	ASN        string    `json:"asn"`         // ASN编号
+	Name       string    `json:"name"`        // ASN名称
+	NetBlock   string    `json:"netblock"`    // ASN的网段
+	NumIPs     int       `json:"num_ips"`     // IP数量
+	Country    string    `json:"country"`     // ASN所属国家编码
+	Domain     string    `json:"domain"`      // ASN所属者的域名
+	CreateTime time.Time `json:"create_time"` // 任务的创建时间
+	Runtime    int       `json:"run_time"`    // 任务的运行时间(单位:秒)
 }
 
-// IPPrefix 结构体
-type IPPrefix struct {
-	Netblock string `files:"netblock"`
-	ID       string `files:"id"`
-	Name     string `files:"name"`
-	Country  string `files:"country"`
-	Size     string `files:"size"`
-	Status   string `files:"status"`
-	Domain   string `files:"domain"`
+// IPNetBlock 结构体
+type IPNetBlock struct {
+	NetBlock string `json:"netblock"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Country  string `json:"country"`
+	Size     string `json:"size"`
+	Status   string `json:"status"`
+	Domain   string `json:"domain"`
 }
 
 // ASNInfo ASN结构体
 type ASNInfo struct {
-	ASN       string     `files:"asn"`
-	Name      string     `files:"name"`
-	Country   string     `files:"country"`
-	Allocated string     `files:"allocated"`
-	Registry  string     `files:"registry"`
-	Domain    string     `files:"domain"`
-	NumIPs    int        `files:"num_ips"`
-	Type      string     `files:"type"`
-	Prefixes  []IPPrefix `files:"prefixes"`
+	ASN       string       `json:"asn"`
+	Name      string       `json:"name"`
+	Country   string       `json:"country"`
+	Allocated string       `json:"allocated"`
+	Registry  string       `json:"registry"`
+	Domain    string       `json:"domain"`
+	NumIPs    int          `json:"num_ips"`
+	Type      string       `json:"type"`
+	NetBlocks []IPNetBlock `json:"prefixes"`
 }
 
 // ASNFilter ASN过滤器
@@ -50,10 +50,10 @@ type ASNFilter struct {
 	ASN int
 	// Country ASN所在国家
 	Country string
-	//Netblock ASN网段
-	Netblock string
+	//NetBlock ASN网段
+	NetBlock string
 }
-type BaseIP struct {
+type IPBase struct {
 	ASN  string `json:"asn"`
 	Ct   string `json:"country"`
 	IP   string `json:"ip"`
